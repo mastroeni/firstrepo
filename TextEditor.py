99caf95553d34c965dba6cafe9e03dfe87a3032a
Wojciech <mastroeni@o2.pl>
2015-01-26 21:01:40 +0100

    GUI
    wstęp do gui

--- TextEditor.py
+++ TextEditor.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
+from tkinter import *
 
-import tkinter
-top = tkinter.Tk()
+def closeWindow(zdarznie):
+    root.quit() # przerywa tryb nasluchiwania zdarzen
+    root.destroy() # zwalnia zasoby
+
+root = Tk()
 # Code to add widgets will go here...
-top.mainloop()
+root.geometry('500x300') # rozmiar okna
+root.title("This is a title") # tytul
+
+labelek = Label(root, text="Mos", font=("Arial",24,"italic"),foreground="yellow",background="blue") #ustawienia okna, zolte litery na niebieskim tle
+labelek.pack(expand=YES, fill=BOTH) # fill(poziomo,pionowo, na dwie strony), expand (rozszerza sie wraz z powiekszeniem okna)
+
+buttonek = Button(root, text="Close window", background="red", command=closeWindow) #(root=Tk(), tekst na oknie, tlo przycisku, funkcja wywolana)
+buttonek.pack() # umieszcza przycisk (side = LEFT,BOTTOM, etc, fill-rozszerza)
+buttonek.bind("<Enter>", closeWindow) # <Enter> - najechanie myszka, <Button> nacisniecie, closeWindow - funkcja
+
+
+root.mainloop() # oczekuje na akcje
